ff866c43cfd9d24b6dd68e89faeaf8181e17c885
boulanouar-amine <boulanouarmohamedamine@gmail.com>
2022-02-15 02:57:32 -0500

    Revert "chaima a"
    This reverts commit 56e76dacf37e4cd869d94c811e115bcd3ae8cbd0.

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estlab.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyProjects.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

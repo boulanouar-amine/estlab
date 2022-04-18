35b14d2fd6c25735d4ae15e6bbeb7529068c31a9
boulanouar-amine <boulanouarmohamedamine@gmail.com>
2022-04-18 13:07:37 +0000

    premier

--- estlab/easymath/views.py
+++ estlab/easymath/views.py
@@ -3,54 +3,61 @@ import re
 import array_to_latex as a2l
 import numpy as np
 import sympy as sp
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 commands = ("calculate", "inverse", "transpose", "determinant",
             "trace", "vector_difference", "average", "valeur propre", "nombre_parfait",
-            "intervalle_parfait", "nombre_premier","derivée", "primitive","integrale")
+            "intervalle_parfait", "nombre_premier", "derivée", "primitive", "integrale")
 
 mat = np.zeros((1, 1))
+premier=""
 
 # la premier fonction qui contient le get
 
 def run(request):
-    global mat,commands
+    global mat, commands,premier
     res = "bonjour veuiller entrer help pour la syntax"
     try:
 
         if request.method == 'GET':
 
-
             if request.GET.get("upload") == "upload+file":
                 res = format_all(0)
-
 
             if request.GET.get("matrice") == "matrice":
                 res = format_all(0)
             else:
-                res = ''.join([eval("format_all(" + ele.replace(" ","_") + "(mat))") for ele in commands if str(request.GET.get(ele)) == str(ele)])
-
-        if request.method == 'POST':
-            command = request.POST["cal"]
+                res = ''.join([eval("format_all(" + ele.replace(" ", "_") + "(mat))") for ele in commands if
+                               str(request.GET.get(ele)) == str(ele)])
 
 
-            if re.search("x", command):
 
-                if request.GET.get("derivée") == "derivée":
-                   res = str(derivée(command))
 
-                if request.GET.get("primitive") == "primitive":
-                    res = str(primitive(command))
+        elif request.method == 'POST':
 
-                if request.GET.get("integrale") == "integrale":
-                    res = str(integrale(command))
+            try :
+                premier =  request.POST["nbrP"]
+                premier = str(nombre_premier(request.POST["nbrP"]))
 
-            else:
-                        res = format_all(calculate((command)))
+            except:
+                command = request.POST["cal"]
 
-# res = ''.join([eval("str(" + ele + "(command))") for ele in commands if str(request.GET.get(ele)) == str(ele)])
-# res = ''.join([eval("format_all(" + ele + "(extract(command,ele)))") for ele in commands if re.search(ele, command)])
+                if re.search("x", command):
 
+                    if request.GET.get("derivée") == "derivée":
+                        res = str(derivée(command))
+
+                    if request.GET.get("primitive") == "primitive":
+                        res = str(primitive(command))
+
+                    if request.GET.get("integrale") == "integrale":
+                        res = str(integrale(command))
+
+                else:
+                    res = format_all(calculate((command)))
+
+    # res = ''.join([eval("str(" + ele + "(command))") for ele in commands if str(request.GET.get(ele)) == str(ele)])
+    # res = ''.join([eval("format_all(" + ele + "(extract(command,ele)))") for ele in commands if re.search(ele, command)])
 
     except (IndexError, ZeroDivisionError):
         res = "division par 0"
@@ -63,54 +70,65 @@ def run(request):
 
         res = ""
 
-    return render(request, 'index.html', {'output': str(res),'mat': str(mat)})
-
+    return render(request, 'index.html', {'output': str(res), 'mat': str(mat),'premier':str(premier)})
 
 
 def Home(request):
     return render(request, 'Home.html')
 
+
 def Matrice(request):
     return render(request, 'Matrice.html')
+
 
 def Contact(request):
     return render(request, 'Contact.html')
 
+
 def Services(request):
     return render(request, 'Services.html')
+
 
 def Statistique(request):
     return render(request, 'Statistique.html')
 
+
 def Fraction(request):
     return render(request, 'Fraction.html')
 
-def NombreAmi(request):
-    return render(request, 'NombreAmi.html')
 
 def NombreAmi(request):
     return render(request, 'NombreAmi.html')
+
+
+def NombreAmi(request):
+    return render(request, 'NombreAmi.html')
+
 
 def NombreParfait(request):
     return render(request, 'NombreParfait.html')
 
+
 def NombrePremier(request):
     return render(request, 'NombrePremier.html')
 
+
 def Syntax(request):
     return render(request, 'Syntax.html')
+
 
 def Probabilite(request):
     return render(request, 'Probabilite.html')
 
 
-
 def calculate(chaine):
+    global mat
     if re.search("]", chaine):
         chaine = matrix_calculator(chaine)
 
     else:
         chaine = eval(chaine)
+        mat = chaine
     return chaine
 
 
@@ -125,11 +143,12 @@ def format_all(chaine):
     elif type(chaine) is str:
         return chaine
 
-    elif  type(chaine) is sp.latex:
+    elif type(chaine) is sp.latex:
         return str("$$" + chaine + "$$")
 
     else:
         return str("$$" + "{:.2f}".format(chaine) + "$$")
+
 
 """
 def extract(command, ele):
@@ -140,6 +159,7 @@ def extract(command, ele):
 
     return chaine
 """
+
 
 # matrix functions
 
@@ -162,12 +182,11 @@ def transpose(chaine):
     chaine = chaine.transpose()
     return chaine
 
-def matrix_calculator(chaine):
 
+def matrix_calculator(chaine):
     op = {"/": np.divide, "\+": np.add, "\*": np.multiply, "\-": np.subtract}
     for ele in op:
         if re.search(ele, chaine):
-
             chaine = chaine.split(ele.strip("\\"))
             a = np.matrix(chaine[0])
             b = np.matrix(chaine[1])
@@ -177,10 +196,11 @@ def matrix_calculator(chaine):
     else:
         chaine = np.matrix(chaine)
         return chaine
+
+
 # vectores
 
 def valeur_propre(chaine):
-
     chaine = np.linalg.eigvals(chaine)
     chaine = np.sort(chaine)
     chaine = np.matrix(chaine)
@@ -194,8 +214,6 @@ def vector_difference(chaine):
 
 
 # statistique
-
-
 
 
 def average(chaine):
@@ -214,9 +232,9 @@ def nombre_parfait(chaine):
             sum += i
 
     if num == sum:
-        return "le nombre " + str(num)  +" est parfait"
+        return "le nombre " + str(num) + " est parfait"
     else:
-        return "le nombre " + str(num)  +" n'est pas parfait"
+        return "le nombre " + str(num) + " n'est pas parfait"
 
 
 def nombre_premier(chaine):
@@ -231,8 +249,8 @@ def nombre_premier(chaine):
 
 def intervalle_parfait(chaine):
     chaine = chaine.split(",")
-    start  = int(chaine[0])
-    end    = int(chaine[1])
+    start = int(chaine[0])
+    end = int(chaine[1])
     res = []
     for num in range(start, end + 1):
         sum = 0
@@ -242,37 +260,36 @@ def intervalle_parfait(chaine):
                 sum += i
 
         if num == sum:
-           res.append(sum)
+            res.append(sum)
     res = np.asmatrix(res)
     return res
 
-#derivee
+
+# derivee
 
 def derivée(mat):
-
     x = sp.symbols('x')
-    y=mat
-    F=sp.diff(y,x)
+    y = mat
+    F = sp.diff(y, x)
 
     return str("$$" + sp.latex(F) + "$$")
 
 
-#primitives
+# primitives
 
 def primitive(mat):
     x = sp.symbols('x')
     F = sp.integrate(mat)
     return str("$$" + sp.latex(F) + "$$")
 
-#integrale
+
+# integrale
 
 def integrale(mat):
     mat = mat.split(",")
     x = sp.symbols('x')  # utiliser la variable x pour mes calculs.
     y = mat[0]
-    F=sp.integrate(y, (x, mat[1], mat[2])) #integrale de y dan l intervale {1 , 3 }
+    F = sp.integrate(y, (x, mat[1], mat[2]))  # integrale de y dan l intervale {1 , 3 }
     return str("$$" + sp.latex(F) + "$$")
 
-
-
-#<!--{% if request.get_full_path == "/?derivée=derivée" or request.get_full_path == "/index?primitive=primitive"  or request.get_full_path == "/index?clr=clr"  or request.get_full_path == "/index?%2B=%2B"  or request.get_full_path == "/index?-=-"  or request.get_full_path == "/index?x=x"  or request.get_full_path == "/index?%2F=%2F"   or request.get_full_path == "/index?%5E=%5E"  or request.get_full_path == "/index?ln=ln"  or  request.get_full_path == "/index?e=e"  or request.get_full_path == "/index?%28=%28"  or request.get_full_path == "/index?%29=%29" %}-->  <form method='get'>
+# <!--{% if request.get_full_path == "/?derivée=derivée" or request.get_full_path == "/index?primitive=primitive"  or request.get_full_path == "/index?clr=clr"  or request.get_full_path == "/index?%2B=%2B"  or request.get_full_path == "/index?-=-"  or request.get_full_path == "/index?x=x"  or request.get_full_path == "/index?%2F=%2F"   or request.get_full_path == "/index?%5E=%5E"  or request.get_full_path == "/index?ln=ln"  or  request.get_full_path == "/index?e=e"  or request.get_full_path == "/index?%28=%28"  or request.get_full_path == "/index?%29=%29" %}-->  <form method='get'>
